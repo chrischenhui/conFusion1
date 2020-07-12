46ead16b0b9185b4aa8359cf77325a79d9250d27
chrischenhui <e0322956@u.nus.edu>
2020-07-13 06:38:50 +0800

    Basic services

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,7 +9,7 @@ import { DISHES } from '../shared/dishes';
 })
 export class MenuComponent implements OnInit {
   
-  dishes: Dish[] = DISHES;
+  dishes: Dish[];
 
   selectedDish: Dish;
   
@@ -17,9 +17,10 @@ export class MenuComponent implements OnInit {
     this.selectedDish = dish;
   }
   
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
 
 }
